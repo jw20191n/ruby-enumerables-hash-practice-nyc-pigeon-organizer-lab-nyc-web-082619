e38bbc0c8407be2e4365237b1c6076dbe974eed9
Jiayi Wang <clairejw1015@gmail.com>
2019-08-22 15:28:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,10 +15,8 @@ def nyc_pigeon_organizer(data)
          if array.include?(name)
            pigeon[name][attributes].push(details.to_s)
          end
-         
       end
-      
-    
+  
     end
   end
   
