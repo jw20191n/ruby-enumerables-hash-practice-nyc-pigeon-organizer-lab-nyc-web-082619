10b590f4ecbe0ba90d09b15eaabfbe24c82701e1
Jiayi Wang <clairejw1015@gmail.com>
2019-08-22 14:49:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   pigeon = { }
   
-  data.each do | key, attribute |
-    key[:gender]
+  data.each do | first_level, second_level |
+    first
     pigeon[name] = { :color => [ ], :gender => [ ], :lives => [ ] }
   end
   
